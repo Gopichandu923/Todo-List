040c672d41d22a85a7df60f3ef0965eed12a0f80
Gopichandu923 <gopichandu6080@gmail.com>
2025-09-10 23:16:39 +0530

    updated the page.jsx for adding buttons to display tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+"use client";
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="justify-items-center">
+      <div className="flex flex-row justify-center  p-2 bg-orange-400 rounded-md">
+        <button className="p-2  rounded-xl">Tasks</button>
+        <button className="p-2  rounded-xl">Completed</button>
+      </div>
+      <div>
+        <h1>Task 1</h1>
+        <h1>Task 2</h1>
+      </div>
+    </div>
+  );
 }
