c2b47f222c6cf7c0dfd37148d6d11a5de587dcc0
Gopichandu923 <gopichandu6080@gmail.com>
2025-09-20 22:29:22 +0530

    Created page for completed missions

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
   return (
     <div className="justify-items-center">
       <div className="flex flex-row justify-center  p-2 bg-orange-400 rounded-md">
-        <button className="p-2  rounded-xl">Tasks</button>
-        <button className="p-2  rounded-xl">Completed</button>
+        <button className="p-2  rounded-xl">Missions</button>
+        <button className="p-2  rounded-xl">Done</button>
       </div>
       <div>
         <h1>Task 1</h1>
