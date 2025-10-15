666b2edf5cfec1c2a07febf7c34de8fd29313db1
Gopichandu923 <gopichandu6080@gmail.com>
2025-10-15 23:08:26 +0530

    updated the register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,17 @@
+"use client";
 const Register = () => {
+  const handleLogin = () => {};
   return (
-    <div className="flex">
+    <div className="flex justify-center ">
       <h1>Register Here</h1>
+      <div>
+        <form>
+          <input type="email" placeholder="Enter your email"></input>
+          <input type="password" placeholder="Enter the password"></input>
+          <input type="password" placeholder="Re-Enter the password"></input>
+          <button onClick={handleLogin}>Register</button>
+        </form>
+      </div>
     </div>
   );
 };
