1d1ec66048502b559962cef2a765a673dc75064b
Gopichandu923 <gopichandu6080@gmail.com>
2025-10-18 21:58:10 +0530

    updated the login by integrating the firebase signin

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,50 @@
+"use client";
+import { useState } from "react";
+import { useAuth } from "../../contexts/AuthContext.js";
+import { useRouter } from "next/navigation";
+
 const Login = () => {
-  return <div className="flex ">Login Here</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signIn, user } = useAuth();
+  const Router = useRouter();
+
+  const handleLogin = async (e) => {
+    console.log(email, password, user);
+    e.preventDefault();
+    try {
+      await signIn(email, password);
+      Router.push("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <h1>Login Here</h1>
+      <form>
+        <label htmlFor="email"></label>
+        <input
+          type="email"
+          value={email}
+          placeholder="Enter email"
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>{" "}
+        <br />
+        <br />
+        <label htmlFor="password"></label>
+        <input
+          type="password"
+          value={password}
+          placeholder="Enter password"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>{" "}
+        <br />
+        <br />
+        <button onClick={handleLogin}>Sign in</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
