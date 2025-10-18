3221049ad07b8825366f71c58711cb2eb2193e78
Gopichandu923 <gopichandu6080@gmail.com>
2025-10-18 21:56:29 +0530

    created the auth context

---
+++ contexts/AuthContext.js
@@ -0,0 +1,60 @@
+"use client";
+
+import React, { createContext, useContext, useEffect, useState } from "react";
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../firebase.js";
+
+// Create the Auth context
+const AuthContext = createContext(null);
+
+// Custom hook for easy access
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+}
+
+// Provider component
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Sign Up with Email and Password
+  const signUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // Sign In with Email and Password
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // Log Out
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  // Listen for changes in the authentication state
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const value = { user, loading, signUp, signIn, logOut };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
