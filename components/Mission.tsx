8e0dfab4cfddf75252795c9d47d7ef2c5e7b3363
Gopichandu923 <gopichandu6080@gmail.com>
2025-10-18 23:38:56 +0530

    updated the missions component for adding task to done list

--- components/Mission.tsx
+++ components/Mission.tsx
@@ -2,14 +2,17 @@
 
 import { useState } from "react";
 
-export default function Missions({ tasks }) {
-  console.log(tasks);
+export default function Missions({ tasks, AddToDone }) {
+  const handleComplete = (task) => {
+    AddToDone({ ...task, date: new Date().toISOString });
+  };
   return (
     <div className="flex justify-center">
       {tasks.map((task, ind) => (
         <div key={ind}>
           <h3>{task.title}</h3>
           <p>{task.description}</p>
+          <button onClick={() => handleComplete(task)}>Completed</button>
         </div>
       ))}
     </div>
