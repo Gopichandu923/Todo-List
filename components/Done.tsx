4f2120906faac34157f312326b93fe5f0152d5eb
Gopichandu923 <gopichandu6080@gmail.com>
2025-10-20 15:45:14 +0530

    updated fro integrating remove method

--- components/Done.tsx
+++ components/Done.tsx
@@ -1,12 +1,7 @@
 "use client";
-import { useEffect } from "react";
+import { TiDelete } from "react-icons/ti";
 
-export default function Done({ completedTasks }) {
-  // Optional: log whenever completedTasks changes
-  useEffect(() => {
-    console.log("Completed tasks updated:", completedTasks);
-  }, [completedTasks]);
-
+export default function Done({ completedTasks, RemoveCompletedTask }) {
   return (
     <div className="flex flex-col items-center gap-4 mt-4 w-150">
       {completedTasks.length === 0 ? (
@@ -15,13 +10,18 @@ export default function Done({ completedTasks }) {
         completedTasks.map((task, ind) => (
           <div
             key={ind}
-            className="flex flex-col bg-green-400 rounded-xl p-3 w-150"
+            className="flex flex-row bg-green-400 rounded-xl p-3 w-150 items-center "
           >
-            <h3 className="text-lg text-white font-bold">{task.title}</h3>
-            <p>{task.description}</p>
-            <p className="text-end">
-              Completed at: {task.date ? task.date : "—"}
-            </p>
+            <div className="flex flex-col grow ">
+              <h3 className="text-lg text-white font-bold">{task.title}</h3>
+              <p>{task.description}</p>
+              <p className="text-end">
+                Completed at: {task.date ? task.date : "—"}
+              </p>
+            </div>
+            <div className="grow-0">
+              <TiDelete size={45} onClick={() => RemoveCompletedTask(task)} />
+            </div>
           </div>
         ))
       )}
