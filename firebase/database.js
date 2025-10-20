fadf4a8772331aa0c440149df66c2b9538177867
Gopichandu923 <gopichandu6080@gmail.com>
2025-10-20 22:20:51 +0530

    created the file for  data storage and retrival through firebase

---
+++ firebase/database.js
@@ -0,0 +1,94 @@
+import { db } from "./config";
+import {
+  collection,
+  doc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  getDocs,
+  query,
+  where,
+  and,
+} from "firebase/firestore";
+
+export const addUser = async (name, email, userId) => {
+  try {
+    const docRef = await addDoc(collection(db, "user"), {
+      email,
+      name,
+      userId,
+      createdAt: new Date(),
+    });
+    console.log(docRef);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const addTask = async (title, description, userId) => {
+  try {
+    const docRef = await addDoc(collection(db, "tasks"), {
+      title,
+      description,
+      isCompleted: false,
+      completedAt: null,
+      userId,
+    });
+    console.log(docRef);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const deleteTask = async (id) => {
+  try {
+    const docRef = await deleteDoc(doc(db, "tasks", id));
+    console.log(docRef);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const updateTask = async (id) => {
+  try {
+    const docRef = await updateDoc(doc(db, "tasks", id), {
+      isCompleted: true,
+      completedAt: new Date(),
+    });
+    console.log(docRef);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getTasks = async (userId) => {
+  try {
+    const docRef = await getDocs(
+      query(
+        collection(db, "tasks"),
+        where("userId", "==", userId),
+        where("isCompleted", "==", false)
+      )
+    );
+    const tasks = docRef.docs.map((d) => ({ id: doc.id, ...doc.data() }));
+    return tasks;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getCompletedTasks = async (userId) => {
+  try {
+    const docRef = await getDocs(
+      query(
+        collection(db, "tasks"),
+        where("userId", "==", userId),
+        where("isCompleted", "==", true)
+      )
+    );
+    const tasks = docRef.docs.map((d) => ({ id: doc.id, ...doc.data() }));
+    return tasks;
+  } catch (err) {
+    console.log(err);
+  }
+};
