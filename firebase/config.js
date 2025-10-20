8eb467bd046ad25147818fd733c9ee3ed17c4cec
Gopichandu923 <gopichandu6080@gmail.com>
2025-10-20 22:19:19 +0530

    renamed the file firebase as config

---
+++ firebase/config.js
@@ -0,0 +1,27 @@
+// firebase/client.js
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+import dotenv from "dotenv";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase for the client side
+const app = initializeApp(firebaseConfig);
+
+// Get the authentication instance
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
+// You would typically get these values from your Firebase Project Settings.
+// Make sure to use NEXT_PUBLIC_ prefix for environment variables accessible on the client.
