c5f1b6ff184d23ba4f7fe5ff5976b127d92bc84c
Gopichandu923 <gopichandu6080@gmail.com>
2025-10-21 18:58:41 +0530

    updated the missions completed component

--- components/Done.tsx
+++ components/Done.tsx
@@ -1,26 +1,37 @@
 "use client";
+
 import { TiDelete } from "react-icons/ti";
 
-export default function Done({ completedTasks, RemoveCompletedTask }) {
+interface Task {
+  id: string;
+  title: string;
+  description: string;
+  completedAt?: string; // optional completed date
+}
+
+export default function Done({ Tasks, RemoveCompletedTask }) {
+  console.log(Tasks);
   return (
     <div className="flex flex-col items-center gap-4 mt-4 w-150">
-      {completedTasks.length === 0 ? (
+      {Tasks.length === 0 ? (
         <p>No completed missions yet.</p>
       ) : (
-        completedTasks.map((task, ind) => (
+        Tasks.map((task) => (
           <div
-            key={ind}
-            className="flex flex-row bg-green-400 rounded-xl p-3 w-150 items-center "
+            key={task.id}
+            className="flex flex-row bg-green-400 rounded-xl p-3 w-150 items-center"
           >
-            <div className="flex flex-col grow ">
+            <div className="flex flex-col grow">
               <h3 className="text-lg text-white font-bold">{task.title}</h3>
               <p>{task.description}</p>
-              <p className="text-end">
-                Completed at: {task.date ? task.date : "—"}
-              </p>
+              <p className="text-end">Completed at: {task.completedAt}</p>
             </div>
             <div className="grow-0">
-              <TiDelete size={45} onClick={() => RemoveCompletedTask(task)} />
+              <TiDelete
+                size={45}
+                className="cursor-pointer"
+                onClick={() => RemoveCompletedTask(task)}
+              />
             </div>
           </div>
         ))
