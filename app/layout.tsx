96e1b4dbcac775ea1acd7aee7ba1bbe54be69ec3
Gopichandu923 <gopichandu6080@gmail.com>
2025-10-21 18:57:39 +0530

    removed the navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import { FaRegUserCircle } from "react-icons/fa";
 import { AuthProvider } from "../contexts/AuthContext";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,14 +23,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 min-h-screen`}
       >
-        <header className="flex flex-row justify-between p-4 text-green-400 text-2xl font-bold">
-          <Link href="/">Todo</Link>
-          <h1>Welcome Gopi</h1>
-          <FaRegUserCircle />
-        </header>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <Navbar />
+          <main className="p-4">{children}</main>
+        </AuthProvider>
       </body>
     </html>
   );
